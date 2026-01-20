3c09a09c8bfef2d8912dd7c346016eb43a9727ba
mrcode <ahmed.mrcode@gmail.com>
2026-01-20 16:45:05 +0200

    posts controller and route

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\PostService;
+use App\Http\Requests\PostRequest;
+use App\Http\Resources\PostResource;
+
+class PostsController extends Controller
+{
+    public function __construct(
+        protected PostService $service
+    ) {}
+
+    public function index()
+    {
+        $list = $this->service->list();
+        return PostResource::collection($list);
+    }
+
+    public function store(PostRequest $request)
+    {
+        $post = $this->service->store($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post created successfully',
+            'data' => $post
+        ], 201);
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        $post = $this->service->update($id, $request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post updated successfully',
+            'data' => $post
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $this->service->delete($id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Category deleted successfully'
+        ]);
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('posts', PostsController::class);
+});
